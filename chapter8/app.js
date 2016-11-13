7b6876ecb0ee77d2550d1db9412947031a880853
Liugenggeng <m14743163347@163.com>
2016-11-13 13:40:41 +0800

    获取客户端请求信息，客户端表单数据发送过来触发data事件，参数data为发送过来的事件

--- chapter8/app.js
+++ chapter8/app.js
@@ -7,6 +7,13 @@ var server = http.createServer(function(req,res) {
 		out.write('客户端请求所用的url字符串为:' + req.url + '\r\n');
 		out.write('客户端请求头对象为:' + JSON.stringify(req.headers) + '\r\n');
 		out.end('客户端请求所用HTTP版本为:' + req.httpVersion);
+		//全段表单发送数据发过来触发data事件，参数data为发送过来的数据
+		req.on('data',function(data) {
+			console.log('服务器已经接受到数据',decodeURIComponent(data))
+		});
+		req.on('end',function() {
+			console.log('客户端请求数据已全部接收完毕')
+		});
 	}
 	res.end();
 }).listen(1334,'127.0.0.1');
