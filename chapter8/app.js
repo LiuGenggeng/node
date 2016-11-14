11e51e4dbcebfb5e553fbaa6df69dfc6c823518b
Liugenggeng <m14743163347@163.com>
2016-11-14 19:52:54 +0800

    创建HTTP服务器

--- chapter8/app.js
+++ chapter8/app.js
@@ -3,28 +3,35 @@ var fs = require('fs');
 var url = require('url');
 var server = http.createServer(function(req,res) {
 	if(req.url !== "/favicon.ico") {
-		var out = fs.createWriteStream('./request.log');
-		out.write('客户端请求所用的方法为:' + req.method + '\r\n');
-		out.write('客户端请求所用的url字符串为:' + req.url + '\r\n');
-		out.write('客户端请求头对象为:' + JSON.stringify(req.headers) + '\r\n');
-		out.end('客户端请求所用HTTP版本为:' + req.httpVersion);
-		//全段表单发送数据发过来触发data事件，参数data为发送过来的数据
-		req.on('data',function(data) {
-			console.log('服务器已经接受到数据',decodeURIComponent(data))
-		});
-		req.on('end',function() {
-			console.log('客户端请求数据已全部接收完毕')
-		});
-		res.write('<html><head><meta charset="utf-8"/></head>');
-		var url_parts = url.parse(req.url);
-		switch(url_parts.pathname) {
-			case './':
-			case './index.html':
-				res.write('<body>首页</body></html>');
-				break;
-			default:
-				res.write('<body>当前访问页面为:' + url_parts.pathname + '.</body></html>');
-		}
+		// var out = fs.createWriteStream('./request.log');
+		// out.write('客户端请求所用的方法为:' + req.method + '\r\n');
+		// out.write('客户端请求所用的url字符串为:' + req.url + '\r\n');
+		// out.write('客户端请求头对象为:' + JSON.stringify(req.headers) + '\r\n');
+
+		
+		// out.end('客户端请求所用HTTP版本为:' + req.httpVersion);
+		// //全段表单发送数据发过来触发data事件，参数data为发送过来的数据
+		// req.on('data',function(data) {
+		// 	console.log('服务器已经接受到数据',decodeURIComponent(data))
+		// });
+		// req.on('end',function() {
+		// 	console.log('客户端请求数据已全部接收完毕')
+		// });
+		// res.write('<html><head><meta charset="utf-8"/></head>');
+		// var url_parts = url.parse(req.url);
+		// switch(url_parts.pathname) {
+		// 	case './':
+		// 	case './index.html':
+		// 		res.write('<body>首页</body></html>');
+		// 		break;
+		// 	default:
+		// 		res.write('<body>当前访问页面为:' + url_parts.pathname + '.</body></html>');
+		// }
+		// res.statusCode = 404;
+		// res.sendDate = true;
+		// res.setHeader('Content-Type','text/html');
+		// res.write('<html><head><meta charset="utf-8"/></head>');
+		// res.write('你好');
 	}
 	res.end();
 }).listen(1334,'127.0.0.1');
