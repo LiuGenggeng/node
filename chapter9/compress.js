8b02076be6d2b79063841a0e90bae9bf05603419
Liugenggeng <m14743163347@163.com>
2016-11-21 22:54:08 +0800

    node.js压缩算法

---
+++ chapter9/compress.js
@@ -0,0 +1,14 @@
+var zlib = require('zlib');
+var fs = require('fs');
+var out = fs.createWriteStream('compress.log');
+var input = 'asdfghjklqwertyuiopzxcvnbm';
+zlib.gzip(input,function(err,buffer) {
+	if(!err) {
+		zlib.unzip(buffer,function(err,buffer) {
+			if(!err) {
+				console.log(buffer.toString());
+				out.write(buffer.toString());
+			}
+		})
+	}
+})
